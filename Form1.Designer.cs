4a7ad51bd73407e2e753c8fc5c65df54430a0a43
edsix66 <132411036+edsix66@users.noreply.github.com>
2024-10-18 22:03:51 -0400

    Update Agenda

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvContactos = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtMovil = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.cmbGenero = new System.Windows.Forms.ComboBox();
+            this.cmbEstadoCivil = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvContactos
+            // 
+            this.dgvContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContactos.Location = new System.Drawing.Point(332, 17);
+            this.dgvContactos.Name = "dgvContactos";
+            this.dgvContactos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvContactos.Size = new System.Drawing.Size(735, 364);
+            this.dgvContactos.TabIndex = 0;
+            this.dgvContactos.Text = "dataGridView1";
+            this.dgvContactos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvContactos_CellContentClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(369, 392);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(170, 52);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(545, 392);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(170, 52);
+            this.btnModificar.TabIndex = 1;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(721, 392);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(170, 52);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(897, 392);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(170, 52);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(183, 378);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(100, 23);
+            this.txtCorreo.TabIndex = 2;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(183, 333);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 23);
+            this.txtTelefono.TabIndex = 3;
+            // 
+            // txtMovil
+            // 
+            this.txtMovil.Location = new System.Drawing.Point(183, 290);
+            this.txtMovil.Name = "txtMovil";
+            this.txtMovil.Size = new System.Drawing.Size(100, 23);
+            this.txtMovil.TabIndex = 4;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(183, 159);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 23);
+            this.txtDireccion.TabIndex = 6;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(183, 67);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 23);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(183, 17);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 23);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(183, 114);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(100, 23);
+            this.dtpFechaNacimiento.TabIndex = 9;
+            // 
+            // cmbGenero
+            // 
+            this.cmbGenero.FormattingEnabled = true;
+            this.cmbGenero.Items.AddRange(new object[] {
+            "Hombre",
+            "Mujer"});
+            this.cmbGenero.Location = new System.Drawing.Point(183, 201);
+            this.cmbGenero.Name = "cmbGenero";
+            this.cmbGenero.Size = new System.Drawing.Size(100, 23);
+            this.cmbGenero.TabIndex = 10;
+            // 
+            // cmbEstadoCivil
+            // 
+            this.cmbEstadoCivil.FormattingEnabled = true;
+            this.cmbEstadoCivil.Items.AddRange(new object[] {
+            "Casado/a",
+            "Soltero/a"});
+            this.cmbEstadoCivil.Location = new System.Drawing.Point(183, 249);
+            this.cmbEstadoCivil.Name = "cmbEstadoCivil";
+            this.cmbEstadoCivil.Size = new System.Drawing.Size(100, 23);
+            this.cmbEstadoCivil.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(22, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 21);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(22, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 21);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(22, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 21);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Fecha de Nacimiento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(22, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 21);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Direccion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(22, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Genero";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(22, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 21);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Estado Civil";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(22, 292);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 21);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Movil";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(22, 335);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 21);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Telefono";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(22, 380);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(138, 21);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Correo Electronico";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(302, 392);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(61, 52);
+            this.btnLimpiar.TabIndex = 13;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbEstadoCivil);
+            this.Controls.Add(this.cmbGenero);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtMovil);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dgvContactos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvContactos;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtMovil;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.ComboBox cmbGenero;
+        private System.Windows.Forms.ComboBox cmbEstadoCivil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarContactos();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            ConexionDB conexionDB = new ConexionDB();
+
+            using (SqlCommand cmd = new SqlCommand("INSERT INTO Contactos (Nombre, Apellido, FechaNacimiento, Direccion, Genero, EstadoCivil, Movil, Telefono, CorreoElectronico) VALUES (@Nombre, @Apellido, @FechaNacimiento, @Direccion, @Genero, @EstadoCivil, @Movil, @Telefono, @CorreoElectronico)", conexionDB.AbrirConexion()))
+            {
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@Apellido", txtApellido.Text);
+                cmd.Parameters.AddWithValue("@FechaNacimiento", dtpFechaNacimiento.Value);
+                cmd.Parameters.AddWithValue("@Direccion", txtDireccion.Text);
+                cmd.Parameters.AddWithValue("@Genero", cmbGenero.SelectedItem.ToString());
+                cmd.Parameters.AddWithValue("@EstadoCivil", cmbEstadoCivil.SelectedItem.ToString());
+                cmd.Parameters.AddWithValue("@Movil", txtMovil.Text);
+                cmd.Parameters.AddWithValue("@Telefono", txtTelefono.Text);
+                cmd.Parameters.AddWithValue("@CorreoElectronico", txtCorreo.Text);
+
+                cmd.ExecuteNonQuery();
+            }
+
+
+            
+            conexionDB.CerrarConexion();
+            MessageBox.Show("Registro insertado con éxito");
+
+            CargarContactos();
+
+        }
+
+
+
+        private void CargarContactos()
+        {
+            ConexionDB conexionDB = new ConexionDB();
+
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Contactos", conexionDB.AbrirConexion()))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                System.Data.DataTable dt = new System.Data.DataTable();
+                da.Fill(dt);
+                dgvContactos.DataSource = dt;  // Cargamos los datos en el DataGridView
+             
+            }
+
+            conexionDB.CerrarConexion();
+        }
+
+        private void dgvContactos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            CargarContactos();
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            ConexionDB conexionDB = new ConexionDB();
+
+            using (SqlCommand cmd = new SqlCommand("UPDATE Contactos SET Nombre = @Nombre, Apellido = @Apellido, FechaNacimiento = @FechaNacimiento, Direccion = @Direccion, Genero = @Genero, EstadoCivil = @EstadoCivil, Movil = @Movil, Telefono = @Telefono, CorreoElectronico = @CorreoElectronico WHERE CorreoElectronico = @Correo", conexionDB.AbrirConexion()))
+            {
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@Apellido", txtApellido.Text);
+                cmd.Parameters.AddWithValue("@FechaNacimiento", dtpFechaNacimiento.Value);
+                cmd.Parameters.AddWithValue("@Direccion", txtDireccion.Text);
+                cmd.Parameters.AddWithValue("@Genero", cmbGenero.SelectedItem.ToString());
+                cmd.Parameters.AddWithValue("@EstadoCivil", cmbEstadoCivil.SelectedItem.ToString());
+                cmd.Parameters.AddWithValue("@Movil", txtMovil.Text);
+                cmd.Parameters.AddWithValue("@Telefono", txtTelefono.Text);
+                cmd.Parameters.AddWithValue("@CorreoElectronico", txtCorreo.Text);
+                cmd.Parameters.AddWithValue("@Correo", txtCorreo.Text); // Para identificar el contacto
+
+                cmd.ExecuteNonQuery();
+            }
+
+            conexionDB.CerrarConexion();
+            MessageBox.Show("Registro modificado con éxito");
+
+            CargarContactos();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            ConexionDB conexionDB = new ConexionDB();
+            string nombreBuscado = txtNombre.Text.Trim(); // Obtener el nombre del TextBox
+
+            if (string.IsNullOrEmpty(nombreBuscado))
+            {
+                MessageBox.Show("Por favor, ingrese un nombre para buscar.");
+                return; // Salir si el campo está vacío
+            }
+
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Contactos WHERE Nombre LIKE @Nombre", conexionDB.AbrirConexion()))
+            {
+                cmd.Parameters.AddWithValue("@Nombre", "%" + nombreBuscado + "%"); // Usar LIKE para buscar coincidencias
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        // Si se encontró el contacto, llenar los campos
+                        txtNombre.Text = reader["Nombre"].ToString();
+                        txtApellido.Text = reader["Apellido"].ToString();
+                        dtpFechaNacimiento.Value = Convert.ToDateTime(reader["FechaNacimiento"]);
+                        txtDireccion.Text = reader["Direccion"].ToString();
+                        cmbGenero.SelectedItem = reader["Genero"].ToString();
+                        cmbEstadoCivil.SelectedItem = reader["EstadoCivil"].ToString();
+                        txtMovil.Text = reader["Movil"].ToString();
+                        txtTelefono.Text = reader["Telefono"].ToString();
+                        txtCorreo.Text = reader["CorreoElectronico"].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontró el contacto");
+                    }
+                }
+            }
+
+            conexionDB.CerrarConexion();
+            CargarContactos(); // Cargar los contactos nuevamente si es necesario
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            ConexionDB conexionDB = new ConexionDB();
+
+            using (SqlCommand cmd = new SqlCommand("DELETE FROM Contactos WHERE CorreoElectronico = @Correo", conexionDB.AbrirConexion()))
+            {
+                cmd.Parameters.AddWithValue("@Correo", txtCorreo.Text);
+                cmd.ExecuteNonQuery();
+            }
+
+            conexionDB.CerrarConexion();
+            MessageBox.Show("Registro eliminado con éxito");
+            CargarContactos();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            // Limpiar TextBoxes
+            txtNombre.Text = string.Empty;
+            txtApellido.Text = string.Empty;
+            txtDireccion.Text = string.Empty;
+            txtMovil.Text = string.Empty;
+            txtTelefono.Text = string.Empty;
+            txtCorreo.Text = string.Empty;
+
+            // Limpiar ComboBoxes
+            cmbGenero.SelectedIndex = -1; // O puedes usar 0 si deseas seleccionar el primer elemento
+            cmbEstadoCivil.SelectedIndex = -1; // O puedes usar 0 si deseas seleccionar el primer elemento
+
+            // Limpiar DateTimePicker
+            dtpFechaNacimiento.Value = DateTime.Now; // O establece a una fecha predeterminada
+        }
+    }
+
+
+    public class ConexionDB
+    {
+        private SqlConnection conexion;
+
+        public ConexionDB()
+        {
+            string cadenaConexion = @"Server=DESKTOP-JALR7LH;Database=AgendaDB;Integrated Security=True;";
+            conexion = new SqlConnection(cadenaConexion);
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+                conexion.Close();
+        }
+    }
+
+    
+}
